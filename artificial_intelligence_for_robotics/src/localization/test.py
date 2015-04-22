4f41092cbf72e67e86501a40c6f2fef3b270cbf4
Jesus-Arturo Escobedo-Cabello <arturoescobedo.iq@gmail.com>
2015-04-22 00:25:22 -0500

    Adding accurate_move function to test.py

--- artificial_intelligence_for_robotics/src/localization/test.py
+++ artificial_intelligence_for_robotics/src/localization/test.py
@@ -4,7 +4,7 @@ This file is used to test new code on the fly
 @author: Jesus Arturo Escobedo Cabello
 '''
 
-p=[0.2, 0.2, 0.2, 0.2, 0.2]
+p=[0.2, 0.2, 0.5, 0.2, 0.2]
 world=['green', 'red', 'red', 'green', 'green']
 measurements = ['red', 'green']
 pHit = 0.6
@@ -20,9 +20,26 @@ def sense(p,Z):
         q[i]=q[i]/s
     return q
 
+""" Moves the elements contained in the p vector EXACTLY (U) units to the left or to the right. 
+    Assumes a cyclic world where the last item is moved to the begining every time 
+    it gets out of the WORLD.
+    @param p: A python array.
+    @param U: An integer that indicates the number of spaces to be shifted,
+    if it is positive shift to the right. 
+"""
+def accurate_move(p,U):
+    for i in range(abs(U)):
+        if U > 0:
+            p.insert(0, p.pop(len(p)-1))
+        elif U < 0:
+            p.insert(len(p)-1, p.pop(0))
+        else:
+            pass
+    return p
+            
+    
+
 
 if __name__ == '__main__':
     print 'prior= ', p 
-    for i in range(len(measurements)):
-        p=sense(p,measurements[i])
-    print 'posterior=', p
+    print 'posterior=', accurate_move(p,1)
