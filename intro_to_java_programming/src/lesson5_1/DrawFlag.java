203a4d0390276638d1d976da83ab0ca9c265431b
Jesus-Arturo Escobedo-Cabello <arturoescobedo.iq@gmail.com>
2015-04-23 00:55:39 -0500

    Drawing indonesian RED and WHITE flag

---
+++ intro_to_java_programming/src/lesson5_1/DrawFlag.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package lesson5_1;
+
+/**
+ * @author CORE 7
+ *
+ */
+public class DrawFlag {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Flag flagPicture = new Flag(400, 200);
+	}
+
+}
