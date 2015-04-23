0c9a4b73a7f59ddc20b52218f0005e3b9b30a35b
Jesus-Arturo Escobedo-Cabello <arturoescobedo.iq@gmail.com>
2015-04-22 19:44:07 -0500

    Implementing inaccurate move function

--- artificial_intelligence_for_robotics/src/localization/test.py
+++ artificial_intelligence_for_robotics/src/localization/test.py
@@ -4,11 +4,14 @@ This file is used to test new code on the fly
 @author: Jesus Arturo Escobedo Cabello
 '''
 
-p = [0.2, 0.2, 0.5, 0.2, 0.2]
+p = [0.0, 0.0, 1.0, 0.0, 0.0]
 world = ['green', 'red', 'red', 'green', 'green']
 measurements = ['red', 'green']
 pHit = 0.6
 pMiss = 0.2
+pExact = 0.8
+pOvershoot = 0.1
+pUndershoot = 0.1
 
 def sense(p, Z):
     q = []
@@ -37,7 +40,29 @@ def accurate_move(p, U):
 
     return q
 
+""" Moves the elements contained in the p vector (U) units to the left or to the right. 
+    Considers the probabilities of undershooting and overshooting while moving
+    Assumes a cyclic world where the last item is moved to the beginning every time 
+    it gets out of the WORLD.
+    @param p: A python array.
+    @param U: An integer that indicates the number of spaces to be shifted,
+    if it is positive shift to the right. 
+"""
+def inaccurate_move(p, U):
+    q = []
+    for i in range(len(p)):
+        q.append(p[(i - U) % len(p)] * pExact +
+                 p[(i - U + 1) % len(p)] * pUndershoot +
+                 p[(i - U - 1) % len(p)] * pOvershoot)
+                #Using %len(p) has the effect of rolling values
+                #over to the beginning of the list if the index is
+                #greater than (or less than) the number of elements
+                #in the list.
+
+    return q
+
+
 
 if __name__ == '__main__':
     print 'prior= ', p
-    print 'posterior=', accurate_move(p, -3)
+    print 'posterior=', inaccurate_move(p, 1)
