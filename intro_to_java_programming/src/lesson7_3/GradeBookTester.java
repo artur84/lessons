c90b44c0fc840b520562bd49f2ad683d646a59e7
Jesus-Arturo Escobedo-Cabello <arturoescobedo.iq@gmail.com>
2015-04-27 20:05:37 -0500

    Total for One Student: Java Lesson 7.3
    Task-Url:

---
+++ intro_to_java_programming/src/lesson7_3/GradeBookTester.java
@@ -0,0 +1,16 @@
+package lesson7_3;
+
+
+public class GradeBookTester
+{
+    public static void main(String[] args)
+    {
+        GradeBook book = new GradeBook("grades.txt");
+        System.out.println(book.totalForStudent(0));
+        System.out.println("Expected: 37.0");
+        System.out.println(book.totalForStudent(21));
+        System.out.println("Expected: 32.0");
+        System.out.println(book.totalForStudent(22));
+        System.out.println("Expected: -1");
+    }
+}
