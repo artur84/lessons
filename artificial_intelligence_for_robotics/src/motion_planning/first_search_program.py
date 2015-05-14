8535a41d312e0943333bf7935e4e5207719bce1a
Jesus-Arturo Escobedo-Cabello <arturoescobedo.iq@gmail.com>
2015-05-14 18:48:58 -0500

    Replacing first_search_program.py with Sebastian's code, Expansion Grid is Ready

--- artificial_intelligence_for_robotics/src/motion_planning/first_search_program.py
+++ artificial_intelligence_for_robotics/src/motion_planning/first_search_program.py
@@ -3,30 +3,23 @@ Created on 13/05/2015
 
 @author: Arturo Escobedo
 '''
-# ----------
+# -----------
 # User Instructions:
 #
-# Define a function, search() that returns a list
-# in the form of [optimal path length, x, y]. For
-# the grid shown below, your function should output
-# [11, 4, 5].
+# Modify the function search so that it returns
+# a table of values called expand. This table
+# will keep track of which step each node was
+# expanded.
 #
-# If there is no valid path from the start point
-# to the goal, your function should return the string
-# 'fail'
+# Make sure that the initial cell in the grid
+# you return has the value 0.
 # ----------
-from _imaging import path
-from egginst.macho.util import move
-
-# Grid format:
-#   0 = Navigable space
-#   1 = Occupied space
 
 grid = [[0, 0, 1, 0, 0, 0],
-        [0, 0, 1, 0, 0, 0],
-        [0, 0, 0, 0, 1, 0],
-        [0, 0, 1, 1, 1, 0],
-        [0, 0, 0, 0, 1, 0]]
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 1, 0],
+        [0, 0, 1, 0, 1, 0],
+        [0, 0, 1, 0, 1, 0]]
 init = [0, 0]
 goal = [len(grid) - 1, len(grid[0]) - 1]
 cost = 1
@@ -38,59 +31,49 @@ delta = [[-1, 0],    # go up
 
 delta_name = ['^', '<', 'v', '>']
 
-
-    
-
 def search(grid, init, goal, cost):
     # ----------------------------------------
-    # insert code here
+    # modify code below
     # ----------------------------------------
-    gcost = 0    #Initial gvalue is zero.
-    path = [gcost, init[0], init[1]]    #[gcost, row, column]
-    open_list = []    #Items that have to be checked
-    gcosts = []    #Corresponding gcosts for each item in open_list
-    closed_list = []    #Items that were already checked
-    #Add the start point to the open list
-    open_list.append(init)
-    gcosts.append(gcost)    #initial cell will have zero cost.
-    cell = open_list[0]
-    while True :
-        #Check if cell is the goal
-        if cell == goal:
-            path = [gcost, cell[0], cell[1]]
-            return path    #If we find the goal we finish the programm
+    closed = [[0 for row in range(len(grid[0]))] for col in range(len(grid))]
+    closed[init[0]][init[1]] = 1
+    expand = [[-1 for row in range(len(grid[0]))] for col in range(len(grid))]
+
+    x = init[0]
+    y = init[1]
+    g = 0
+    step = 0    # The step at which each node is expanded
+    open = [[g, x, y]]
+    expand[x][y] = step
+    step += 1
+    found = False    # flag that is set when search is complete
+    resign = False    # flag set if we can't find expand
+
+    while not found and not resign:
+        if len(open) == 0:
+            resign = True
         else:
-            #Add it to the closed list
-            closed_list.append(cell)
-            #Delete it from the open list
-            index = open_list.index(cell)
-            open_list.pop(index)
-            gcosts.pop(index)
-            #increase gcost
-            gcost += cost
-            #check cell neighbors
-            neighbors = []
-            for move in delta:
-                possible_neighbor = [cell[0] - move[0], cell[1] - move[1] ]
-                #Check if it is inside the grid
-                if (possible_neighbor[0] >= 0 and possible_neighbor[0] < len(grid)) and (possible_neighbor[1] >= 0 and possible_neighbor[1] < len(grid[0])):
-                    neighbors.append(possible_neighbor)
-                    #Check if this cell is not occupied
-                    if grid[possible_neighbor[0]][possible_neighbor[1]] == 0:
-                        #Check if possble_neighbor is not in the closed_list or open_list
-                        if possible_neighbor not in closed_list:
-                            if possible_neighbor not in open_list:
-                                #Add neighbor to the open list.
-                                open_list.append(possible_neighbor)
-                                gcosts.append(gcost)
-            #Get item with minimum gcost item in the open_list
+            open.sort()
+            open.reverse()
+            next = open.pop()
+            x = next[1]
+            y = next[2]
+            g = next[0]
 
-            if len(open_list)>0:
-                gcost = min(gcosts)
-                cell = open_list[gcosts.index(gcost)]
+            if x == goal[0] and y == goal[1]:
+                found = True
             else:
-                return 'fail'
-
+                for i in range(len(delta)):
+                    x2 = x + delta[i][0]
+                    y2 = y + delta[i][1]
+                    if x2 >= 0 and x2 < len(grid) and y2 >= 0 and y2 < len(grid[0]):
+                        if closed[x2][y2] == 0 and grid[x2][y2] == 0:
+                            g2 = g + cost
+                            open.append([g2, x2, y2])
+                            expand[x2][y2] = step
+                            step += 1
+                            closed[x2][y2] = 1
+    return expand
 
 if __name__ == '__main__':
     print search(grid, init, goal, cost)
