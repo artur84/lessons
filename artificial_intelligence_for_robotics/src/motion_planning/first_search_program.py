196386e61b8648f4635887e3d8fb7fa3bf25028b
Jesus-Arturo Escobedo-Cabello <arturoescobedo.iq@gmail.com>
2015-05-13 21:24:11 -0500

    Adding artificial_intelligence_for_robotics first_search_program.py,

---
+++ artificial_intelligence_for_robotics/src/motion_planning/first_search_program.py
@@ -0,0 +1,121 @@
+'''
+Created on 13/05/2015
+
+@author: Arturo Escobedo
+'''
+# ----------
+# User Instructions:
+#
+# Define a function, search() that returns a list
+# in the form of [optimal path length, x, y]. For
+# the grid shown below, your function should output
+# [11, 4, 5].
+#
+# If there is no valid path from the start point
+# to the goal, your function should return the string
+# 'fail'
+# ----------
+from _imaging import path
+from egginst.macho.util import move
+
+# Grid format:
+#   0 = Navigable space
+#   1 = Occupied space
+
+grid = [[0, 0, 1, 0, 0, 0],
+        [0, 0, 1, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0],
+        [0, 0, 1, 1, 1, 0],
+        [0, 0, 0, 0, 1, 0]]
+init = [0, 0]
+goal = [len(grid) - 1, len(grid[0]) - 1]
+cost = 1
+
+delta = [[-1, 0],    # go up
+         [ 0, -1],    # go left
+         [ 1, 0],    # go down
+         [ 0, 1]]    # go right
+
+delta_name = ['^', '<', 'v', '>']
+
+
+    
+
+def search(grid, init, goal, cost):
+    # ----------------------------------------
+    # insert code here
+    # ----------------------------------------
+    print 'Start search'
+    print 'goal: ', goal
+    gcost = 0    #Initial gvalue is zero.
+    path = [gcost, init[0], init[1]]    #[gcost, row, column]
+    open_list = []    #Items that have to be checked
+    gcosts = []    #Corresponding gcosts for each item in open_list
+    closed_list = []    #Items that were already checked
+    #Add the start point to the open list
+    open_list.append(init)
+    gcosts.append(gcost)    #initial cell will have zero cost.
+    cell = open_list[0]
+    while True :
+        print '----------'
+        print 'current cell'
+        print cell
+        print 'open list'
+        print open_list
+        print 'gcost'
+        print gcosts
+        #Check if cell is the goal
+        if cell == goal:
+            print 'if'
+            path = [gcost, cell[0], cell[1]]
+            return path    #If we find the goal we finish the programm
+        else:
+            #Add it to the closed list
+            closed_list.append(cell)
+            #Delete it from the open list
+            index = open_list.index(cell)
+            open_list.pop(index)
+            gcosts.pop(index)
+            #increase gcost
+            gcost += cost
+            #check cell neighbors
+            neighbors = []
+            for move in delta:
+                possible_neighbor = [cell[0] - move[0], cell[1] - move[1] ]
+                #Check if it is inside the grid
+                if (possible_neighbor[0] >= 0 and possible_neighbor[0] < len(grid)) and (possible_neighbor[1] >= 0 and possible_neighbor[1] < len(grid[0])):
+                    neighbors.append(possible_neighbor)
+                    #Check if this cell is not occupied
+                    if grid[possible_neighbor[0]][possible_neighbor[1]] == 0:
+                        #Check if possble_neighbor is not in the closed_list or open_list
+                        if possible_neighbor not in closed_list:
+                            if possible_neighbor not in open_list:
+                                #Add neighbor to the open list.
+                                open_list.append(possible_neighbor)
+                                gcosts.append(gcost)
+            #Get item with minimum gcost item in the open_list
+
+            if len(open_list)>0:
+                gcost = min(gcosts)
+                cell = open_list[gcosts.index(gcost)]
+            else:
+                print 'fail'
+                return 0
+
+
+
+
+
+
+
+            
+
+
+
+if __name__ == '__main__':
+    path = search(grid, init, goal, cost)
+    print 'Resulting Path:'
+    print path
+    print 'Expected Path:'
+    print [11, 4, 5]
+    pass
